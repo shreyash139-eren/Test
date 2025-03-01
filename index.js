a635aa024c6570873901ce765a7e18d22b6b1ebb
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-03-01 17:41:22 +0530

    connit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(kaapa)
+// kaapa=10*8
+// console.log(kaapa)
+let a=10
+let b=20
+console.log(a*b)
---
+++ my.js
@@ -0,0 +1,5 @@
+let n=5
+let bag=""
+    for(let i=1;i<=n;i++){
+        console.log(bag)
+    }
